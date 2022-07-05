7e4fc7819bdcb279176097653b60037346dca721
Haowei Cheng <Haowei4671@gmail.com>
2022-07-04 23:57:18 -0700

    files of Pang game
    create pang game by follow the oop(object oriented programming)

---
+++ paddle.py
@@ -0,0 +1,20 @@
+from msilib.schema import Class
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self, cor):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(cor[0], cor[1])
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
